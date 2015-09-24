8dcd31800110eee2b2be5cf3c4a906faac5589bf
wangchaofan <514610423@qq.com>
2015-09-24 15:13:24 +0800

    chaofanw

--- 2015/jsEssence/common.js
+++ 2015/jsEssence/common.js
@@ -11,7 +11,30 @@ Function.prototype.method = function(name, func) {
 	}
 	return this;
 };
+/**
+ * 方法扩展, 传递参数创建一个新的函数
+ * @for Function
+ * @return {Function} 新方法
+ */
+Function.method('curry', function() {
+	var slice = Array.prototype.slice,
+		args = slice.apply(arguments),
+		that = this;
+	return function() {
+		return that.apply(null, args.concat(slice.apply(arguments)));
+	}
+});
 
+/**
+ * 集成方法
+ * @for Function
+ * @param  {Function} Parent 父类
+ * @return {Functions}  this;
+ */
+Function.method('inherits', function(Parent) {
+	this.prototype = new Parent();
+	return this;
+});
 /**
  * 数字取整
  * @for Number
@@ -29,6 +52,7 @@ Number.method('integer', function() {
 String.method('trim', function() {
 	return this.replace(/^\s+|\s+$/g, '');
 });
+
 /**
  * 寻找字符串中HTML字符实体并替换
  * @param  {Object} 字符实体表
